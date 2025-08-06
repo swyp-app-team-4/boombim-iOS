ada14bc3a2639a97caf02d3ab088742dafa43fdd
zerohyeon <joyh0808@gmail.com>
2025-08-06 21:06:05 +0900

    feat: add UserDefaultsManager for key-value management

---
+++ BoomBim/Utilities/Storage/UserDefaultsKeys.swift
@@ -0,0 +1,12 @@
+//
+//  UserDefaultsKeys.swift
+//  BoomBim
+//
+//  Created by 조영현 on 8/6/25.
+//
+
+enum UserDefaultsKeys {
+    enum Auth {
+        static let accessToken = "accessToken"
+    }
+}
