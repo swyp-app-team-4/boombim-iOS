ad6c7812021b8689360a1723b5259b8a2f56e85a
zerohyeon <joyh0808@gmail.com>
2025-10-11 00:24:11 +0900

    feat: remove Communication tab from TabBar

--- BoomBim/Presentation/Tabs/MainTabBarCoordinator.swift
+++ BoomBim/Presentation/Tabs/MainTabBarCoordinator.swift
@@ -13,36 +13,35 @@ final class MainTabBarCoordinator: Coordinator {
     
     private var homeCoordinator: HomeCoordinator?
     private var mapCoordinator: MapCoordinator?
-    private var chatCoordinator: ChatCoordinator?
+//    private var chatCoordinator: ChatCoordinator?
     private var myPageCoordinator: MyPageCoordinator?
 
     func start() {
         let homeNC = UINavigationController()
         let mapNC = UINavigationController()
-        let chatNC = UINavigationController()
+//        let chatNC = UINavigationController()
         let myPageNC = UINavigationController()
 
         let homeCoordinator = HomeCoordinator(navigationController: homeNC)
         let mapCoordinator = MapCoordinator(navigationController: mapNC)
-        let chatCoordinator = ChatCoordinator(navigationController: chatNC)
+//        let chatCoordinator = ChatCoordinator(navigationController: chatNC)
         let myPageCoordinator = MyPageCoordinator(navigationController: myPageNC)
         
         self.homeCoordinator = homeCoordinator
         self.mapCoordinator = mapCoordinator
-        self.chatCoordinator = chatCoordinator
+//        self.chatCoordinator = chatCoordinator
         self.myPageCoordinator = myPageCoordinator
         
         homeNC.tabBarItem = UITabBarItem(title: "", image: UIImage.iconHome, selectedImage: UIImage.iconHome)
         mapNC.tabBarItem = UITabBarItem(title: "", image: UIImage.iconMap, selectedImage: UIImage.iconMap)
-        chatNC.tabBarItem = UITabBarItem(title: "", image: UIImage.iconChat, selectedImage: UIImage.iconChat)
+//        chatNC.tabBarItem = UITabBarItem(title: "", image: UIImage.iconChat, selectedImage: UIImage.iconChat)
         myPageNC.tabBarItem = UITabBarItem(title: "", image: UIImage.iconProfile, selectedImage: UIImage.iconProfile)
 
         homeCoordinator.start()
         mapCoordinator.start()
-        chatCoordinator.start()
+//        chatCoordinator.start()
         myPageCoordinator.start()
 
-        tabBarController.viewControllers = [homeNC, mapNC, chatNC, myPageNC]
+        tabBarController.viewControllers = [homeNC, mapNC, /*chatNC,*/ myPageNC]
     }
 }
-
